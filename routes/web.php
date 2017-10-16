57afa44f9f1c7ca247ad9641811c834a1c0f3bbb
Michael Dyrynda <michael@dyrynda.com.au>
2017-10-17 08:30:17 +1030

    add routes, view for basic form with AWS Signature v2

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,31 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', [
+        'policy' => $policy = createPolicy(),
+        'signature' => signPolicy($policy),
+    ]);
 });
+
+Route::get('/s3-upload', function () {
+    return request()->all();
+});
+
+function createPolicy() {
+    return base64_encode(json_encode([
+        'expiration' => now()->addHour()->format('Y-m-d\TG:i:s\Z'),
+        'conditions' => [
+            ['bucket' => config('filesystems.disks.s3.bucket')],
+            ['acl' => 'private'],
+            ['starts-with', '$key', ''],
+            ['eq', '$success_action_redirect', url('/s3-upload')],
+        ],
+    ]));
+}
+
+function signPolicy($policy)
+{
+    return with($policy, function ($policy) {
+        return base64_encode(hash_hmac('sha1', $policy, config('filesystems.disks.s3.secret'), true));
+    });
+}
